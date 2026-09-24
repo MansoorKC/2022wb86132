9b6add3e44062d9e144cc2cbc3785b2765909f1e
Mansoor K C <2022wb86132@wilp.bits-pilani.ac.in>
2026-09-24 19:23:12 +0530

    Fixed HelloWorld and updated CI message

--- src/main/java/com/devops/HelloWorld.java
+++ src/main/java/com/devops/HelloWorld.java
@@ -1,5 +1,5 @@
 package com.devops;
-
+// Updated for Jenkins Continuous Integration
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Continuous Integration!");
